1368dc82f1abec7c138d9b00e93ba1f200ca0c7d
Paul Charensol <charensol.paul@outlook.fr>
2019-03-27 18:01:00 +0100

    Angular02

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+show: boolean;
+
+ageHidden(){
+  this.show = !this.show;
+}
+
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
